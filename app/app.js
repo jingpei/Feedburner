5c0b2e92580d367348d7f7ff3b77082e7f85827d
jingpei <jingjpei@gmail.com>
2015-12-02 17:16:15 -0800

    (feat) Changed the feed source to bittorrent heroku deployment

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-google.load("feeds", "1");
+// google.load("feeds", "1");
 
 var app = angular.module('feedApp', ['ngRoute']);
 
@@ -15,35 +15,36 @@ app.config(function($routeProvider){
     .otherwise({redirectTo: '/'})
 })
 
-app.controller('feedLoader', function($scope, $window, $sce, $location){
+app.controller('feedLoader', function($scope, $window, $http, $sce, $location){
   $scope.entry;
 
   var initialize = function () {
-    var feedUrl = "http://feeds.feedburner.com/tedtalks_video"
-    var entryCount = 20;
-    var feed = new $window.google.feeds.Feed(feedUrl);
-    feed.setNumEntries(entryCount);
-    feed.load(function(result) {
-      if (!result.error) {
-        $scope.results = result.feed;
-        $scope.entries = result.feed.entries;
-        console.log($scope.entries);
-        $scope.$apply();
+    var feedUrl = "https://agile-thicket-5774.herokuapp.com/feed" 
+    
+    $http({
+      method: 'GET',
+      url: feedUrl
+    })
+    .then(function(result){
+      if(!result.error){
+        console.log(result.data.items);
+        window.data = result.data.items[0]
+        $scope.entries = result.data.items;
       }
     });
   };
 
   $scope.$on('$locationChangeStart', function(){
-    console.log($location.$$path);
     if($location.$$path.length > 1){
       var entryIndex = $location.$$path.split('/')[1];
-      var trustedResource = $sce.trustAsResourceUrl($scope.entries[entryIndex].mediaGroups[0].contents[0].url);
-      $scope.entries[entryIndex].mediaGroups[0].contents[0].url = trustedResource;
-      $scope.entries[entryIndex].content = $scope.entries[entryIndex].content.split('<img')[0];
+      console.log($scope.entries[entryIndex])
+      var trustedResource = $sce.trustAsResourceUrl($scope.entries[entryIndex]['media:content']['@url']);
+      $scope.entries[entryIndex]['media:content']['@url'] = trustedResource;
+      $scope.entries[entryIndex].description = $scope.entries[entryIndex].description.split('<img')[0];
       $scope.entry = $scope.entries[entryIndex];
     } 
   })
 
-  $window.google.setOnLoadCallback(initialize);
+  initialize();
 
 });
